28f2420d7776609202f0218dac976a84b4f5e536
ARTHI R <arthiaaru23@gmail.com>
2024-10-24 11:25:11 +0530

    Complete few components and pages

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,13 +1,32 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Components/About';
+import Services from './Components/Services';
+import Skills from './Components/Skills';
+import Projects from './Components/Projects';
+import Contact from './Components/Contact';
+import Navbar from './Components/Navbar';
+import Resume from './Components/Resume';
 
 function App() {
   return (
     <>
-      <div>
-        <h1>Portfolio</h1>
-      </div>
+    <Router>
+    <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/navbar" element={<Navbar />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+    </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
